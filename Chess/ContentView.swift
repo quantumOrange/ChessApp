6be377693767f81ff03cd4234fc28e09a64ac0ba
David Crooks <crodave@gmail.com>
2019-08-09 11:56:35 +0100

    Use geometry of board rather than fixed square size.

--- Chess/ContentView.swift
+++ Chess/ContentView.swift
@@ -28,11 +28,18 @@ enum SquareColor {
     }
 }
 
+class AppState {
+    var chessBoard:ChessBoard =  ChessBoard.start()
+    var selectedSquare:ChessboardSquare? = nil
+}
+
 struct ContentView : View {
-    //@State var chessBoard: ChessBoard
-    
     var body: some View {
+        VStack(alignment: .center, spacing:50){
+        Text("Black Player Name")
         BoardView(board: ChessBoard.start())
+        Text("White Player Name")
+        }
     }
 }
 
@@ -40,26 +47,25 @@ struct BoardView : View {
     @State var board:ChessBoard
     
     var body: some View {
-        
-        HStack(alignment: .center,spacing:0){
-            ForEach((0..<8)) { i in
-               
- 
-                VStack(alignment: .center, spacing:0){
-                    ForEach((0..<8)) { j in
-                      
+        GeometryReader { geometry in
+            HStack(alignment: .center,spacing:0){
+                ForEach((0..<8)) { i in
+                    
+                    
+                    VStack(alignment: .center, spacing:0){
+                        ForEach((0..<8)) { j in
+                            
+                            Square(piece:self.board[i,7-j] ,
+                                   squareColor: SquareColor.at(file: i, rank: 7-j),width:geometry.size.width/8.0)
+                            
+                        }
                         
-                         Square(piece:self.board[i,7-j] ,
-                                squareColor: SquareColor.at(file: i, rank: 7-j))
                         
                     }
                     
-                    
                 }
-
             }
         }
-
     }
 }
 
@@ -67,34 +73,25 @@ struct Square : View {
     @State var selected:Bool = false
     @State var piece:ChessPiece?
     @State var squareColor:SquareColor
+    let width:Length
     var body: some View {
         Button(action:{ self.selected = !self.selected } ){
-            ZStack(){
-                
-                Spacer()
-                    .frame(width: 50, height: 50, alignment: .center)
-                    .background(self.selected ? Color.yellow : squareColor.color  )
-                
                 if piece != nil {
-                    PieceView(piece: piece!)
+                    PieceView(piece: piece!, width:width)
                 }
-                
-                }
-                .frame(width: 50, height: 50, alignment: .center)
-        }
-        
-            
+            }
+            .frame(width:width, height: width, alignment: .center)
+            .background(self.selected ? Color.yellow : squareColor.color  )
     }
 }
 
 struct PieceView : View {
     @State var piece:ChessPiece
-    
+    let width:Length
     var body: some View {
         Text(piece.symbol)
             .font(.largeTitle)
-            .frame(width: 50, height: 50, alignment: .center)
-        
+            .frame(width: width, height: width, alignment: .center)
     }
 }
 
